fc4406468cdcb0ff3234a1b1e40b4142d7fdd3f2
Stephan Laukien <software@laukien.com>
2018-05-03 09:17:22 +0200

    FIX: binary reading (WIN32), add size of data

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 
 	/* input */
 	FILE *in;
-	in = fopen(argv[1], "r");
+	in = fopen(argv[1], "rb");
 	if (!in) {
 		fprintf(stderr, "ERROR:\t\tunable to read input-file\n");
 		return 1;
@@ -27,20 +27,32 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+    /* write 'include' */
+    fprintf(out, "#include <stdlib.h>\n\n");
+
 	/* name of the object */
 	char *obj = strdup(argv[1]);
-	unsigned int i;
+
+    /* remove and convert non-alphanumeric characters */
+	size_t i;
 	for (i = 0; i < strlen(obj); ++i) {
 		if (!isalnum(obj[i])) obj[i] = '_';
+        else obj[i] = tolower(obj[i]);
 	}
+
 	/* convert and write characters */
 	unsigned int c;
-	fprintf(out, "char %s[] = {\n", obj);
+    size_t l = 0;
+	fprintf(out, "char %s_array[] = {\n", obj);
 	while ((c = fgetc(in)) != EOF) {
 		fprintf(out, "'\\x%X', ", (unsigned int) c);
+        ++l;
 	}
-	fprintf(out, "'\\0'");
-	fprintf(out, "};\n");
+	//fprintf(out, "'\\0'");
+	fprintf(out, "0};\n");
+
+    /* write size */
+    fprintf(out, "const size_t %s_size = %zu;\n", obj, l);
 
 	/* free resources */
 	free(obj);
